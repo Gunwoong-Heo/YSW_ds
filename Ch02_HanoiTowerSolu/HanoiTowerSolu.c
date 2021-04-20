f0109f81c85f2922641686be6ce7bca1db44b241
Gunwoong-Heo <gjrjsdnd1129@gmail.com>
2021-04-20 14:36:58 +0900

    코드 실행 순서를 알아보기 위한 printf 코드 추가

--- Ch02_HanoiTowerSolu/HanoiTowerSolu.c
+++ Ch02_HanoiTowerSolu/HanoiTowerSolu.c
@@ -4,12 +4,15 @@ void HanoiTowerMove(int num, char from, char by, char to)
 {
 	if (num == 1)    // 이동할 원반의 수가 1개라면
 	{
+		printf("3\n");
 		printf("원반1을 %c에서 %c로 이동 \n", from, to);
 	}
 	else
 	{
+		printf("1\n");
 		HanoiTowerMove(num - 1, from, to, by);    // 3단계 중 1단계
 		printf("원반%d을(를) %c에서 %c로 이동 \n", num, from, to);  // 3단계 중 2단계
+		printf("2\n");
 		HanoiTowerMove(num - 1, by, from, to);    // 3단계 중 3단계
 	}
 }
@@ -18,6 +21,7 @@ void HanoiTowerMove(int num, char from, char by, char to)
 int main(void)
 {
 	// 막대A의 원반 3개를 막대B를 경유하여 막대C로 옮기기
-	HanoiTowerMove(3, 'A', 'B', 'C');
+	//HanoiTowerMove(3, 'A', 'B', 'C');
+	HanoiTowerMove(5, 'A', 'B', 'C');
 	return 0;
 }
